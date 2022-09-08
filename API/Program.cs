e4e06050e7e7cba98ec8604d564e239c9bae0cd3
ISAC\isacg <isac@webking.se>
2022-09-08 18:14:36 +0200

    Added hostservice and exception in Program.cs in Main method to run host and catch exceptions in case something goes wrong

--- API/Program.cs
+++ API/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +16,18 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            try {
+                var context = services.GetRequiredService<StoreContext>();
+                context.Database.Migrate();
+            } catch (Exception e) {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(e, "Something wrong happened during migration");
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
