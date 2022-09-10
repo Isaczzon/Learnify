b85821b8b977c8d24adf7b1a08c04543380b4933
ISAC\isacg <isac@webking.se>
2022-09-10 21:03:00 +0200

    Added a dropdown-menu with modifications in ../client/src/components/Navigation.tsx and replaced new styling in ../client/src/sass/Components/_navigation.scss

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as FaIcons from "react-icons/fa";
 import Logo from "../Assets/logo.png";
 
 const Navigation = () => {
+    const [sidebar, setSidebar] = useState(false);
+    const showSidebar = () => setSidebar(!sidebar);
+
     return (
         <div className="nav-container">
             <div className="nav">
                 <div className="nav__left">
+                    <div className="nav__left__hamburger">
+                        <FaIcons.FaBars onClick={showSidebar} />
+                        <nav className={sidebar ? 'navmenu active' : 'nav-menu'}>
+                            <ul className="nav-menu-items" onClick={showSidebar}>
+                                <li className="cancel">
+                                    <FaIcons.FaChevronLeft />
+                                </li>
+
+                                <li className="nav-menu-items__header">Navigation</li>
+                                <li>Categories</li>
+                                <li>Courses</li>
+                            </ul>
+                        </nav>
+                    </div>
                     <img className="nav__left__logo" src={Logo} alt="Logo" />
                     <ul className="nav__left__list">
                         <div className="nav__left__list__item">Categories</div>
