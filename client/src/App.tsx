9dc9229a83fafd0453d5e04d7df873a4dec57eeb
ISAC\isacg <isac@webking.se>
2022-09-10 19:05:14 +0200

    Added navigationbar with styling and deleted unessesary imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import './App.css';
 import DetailPage from './pages/DetailPage';
 import Homepage from './pages/Homepage';
 import Login from './pages/Login';
+import Navigation from './components/Navigation';
 
 function App() {
   return <>
+  <Navigation />
     <Switch>
       <Route exact path="/" component={Homepage} />
       <Route exact path="/login" component={Login} />
