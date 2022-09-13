1a849ea05070a006a71b09b9615eaea7ee89c907
ISAC\isacg <isac@webking.se>
2022-09-13 15:11:57 +0200

    Created BaseEntity.cs, Requirement.cs, Learning.cs and modified Course.cs and StoreContext.cs

--- Entity/Course.cs
+++ Entity/Course.cs
@@ -1,8 +1,9 @@
 using System;
+using System.Collections.Generic;
 
 namespace Entity
 {
-    public class Course
+    public class Course : BaseEntity
     {
         public Guid Id { get; set; }
 
@@ -11,5 +12,11 @@ namespace Entity
         public string Instructor { get; set; }
         public decimal Rating { get; set; }
         public string Image { get; set; }
+        public string  SubTitle { get; set; }
+        public string Description { get; set; }
+        public int Students { get; set; }
+        public string Language { get; set; }
+        public string Level { get; set; }
+        public ICollection<Requirement> Requirements { get; set; }
     }
 }
