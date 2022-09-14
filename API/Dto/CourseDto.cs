c5cb380087665057ae59e6f8cd85a764eff3bab4
Isac <isac.gustafsson.syne21lin@tucsweden.se>
2022-09-14 18:41:05 +0200

    Took away DateTime.now in CourseDto since we get it form the db

--- API/Dto/CourseDto.cs
+++ API/Dto/CourseDto.cs
@@ -19,6 +19,6 @@ namespace API.Dto
         public ICollection<RequirementDto> Requirements { get; set; }
         public ICollection<LearningDto> Learnings { get; set; }
         public string Category { get; set; }
-        public DateTime LastUpdated { get; set; } = DateTime.Now;
+        public DateTime LastUpdated { get; set; }
     }
 }
