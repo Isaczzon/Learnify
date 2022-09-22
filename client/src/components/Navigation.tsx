401fed0753fd7433f2e4a685c651cccba13082a2
Isac <isac.gustafsson.syne21lin@tucsweden.se>
2022-09-22 11:09:25 +0200

    Updated methods removeBasketItem and AddToCart and added Link to shoppingcart, also changed a class to correct one and added some styles in _navigation.scss

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,55 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import * as FaIcons from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Logo from "../assets/logo.png";
+import { useStoreContext } from "../context/StoreContext";
 
 const Navigation = () => {
-    const [sidebar, setSidebar] = useState(false);
-    const showSidebar = () => setSidebar(!sidebar);
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
 
-    return (
-        <div className="nav-container">
-            <div className="nav">
-                <div className="nav__left">
-                    <div className="nav__left__hamburger">
-                        <FaIcons.FaBars onClick={showSidebar} />
-                        <nav className={sidebar ? 'navmenu active' : 'nav-menu'}>
-                            <ul className="nav-menu-items" onClick={showSidebar}>
-                                <li className="cancel">
-                                    <FaIcons.FaChevronLeft />
-                                </li>
+  const { basket } = useStoreContext();
+  const basketCount = basket?.items.length;
 
-                                <li className="nav-menu-items__header">Navigation</li>
-                                <li>Courses</li>
-                            </ul>
-                        </nav>
-                    </div>
-                    <img className="nav__left__logo" src={Logo} alt="Logo" />
-                    <ul className="nav__left__list">
-                        <Link to="/">
-                        <div className="nav__left__list__item">Home</div>
-                        </Link>
-                    </ul>
-                </div>
-                <div className="nav__right">
-                    <form className="nav__right__search">
-                        <input 
-                        type="text" 
-                        className="nav__right__search__input"
-                        placeholder="Search Courses..."
-                        />
-                        <button title="Search button" type="button" className="nav__right__search__button">
-                            <FaIcons.FaSearch />
-                        </button>
-                    </form>
-                    <div className="nav__right__cart">
-                        <FaIcons.FaShoppingCart />
-                        <span className="nav__right__cart__count"></span>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="nav-container">
+      <div className="nav">
+        <div className="nav__left">
+          <div className="nav__left__hamburger">
+            <FaIcons.FaBars onClick={showSidebar} />
+            <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+              <ul className="nav-menu-items" onClick={showSidebar}>
+                <li className="cancel">
+                  <FaIcons.FaChevronLeft />
+                </li>
+
+                <li className="nav-menu-items__header">Navigation</li>
+                <li>Courses</li>
+              </ul>
+            </nav>
+          </div>
+          <img className="nav__left__logo" src={Logo} alt="Logo" />
+          <ul className="nav__left__list">
+            <Link to="/">
+              <li className="nav__left__list__item">Home</li>
+            </Link>
+          </ul>
         </div>
-    );
+        <div className="nav__right">
+          <form className="nav__right__search">
+            <input
+              type="text"
+              className="nav__right__search__input"
+              placeholder="Search Courses..."
+            />
+            <button
+              title="Search button"
+              type="button"
+              className="nav__right__search__button"
+            >
+              <FaIcons.FaSearch />
+            </button>
+          </form>
+          <Link to="/basket">
+            <div className="nav__right__cart">
+              <FaIcons.FaShoppingCart />
+              {basketCount! > 0 && (
+                <span className="nav__right__cart__count">{basketCount}</span>
+              )}
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navigation;
