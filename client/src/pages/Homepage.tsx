58cf672ef62c0b0719afb7c9fbcc89764254f983
Isac <isac.gustafsson.syne21lin@tucsweden.se>
2022-09-22 17:51:52 +0200

    Implemeted code for fetching data of product to browser

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,30 +1,33 @@
-import React, {useState, useEffect} from 'react';
-import { Course } from '../models/course';
-import agent from '../actions/agent';
-import {Row} from 'antd';
-import { PaginatedCourse } from '../models/paginatedCourse';
-import ShowCourses from '../components/ShowCourses';
+import React, { useEffect } from "react";
+import { Course } from "../models/course";
+import { Row } from "antd";
+import ShowCourses from "../components/ShowCourses";
+import { useAppDispatch, useAppSelector } from "../redux/store/configureStore";
+import { coursesSelector, getCoursesAsync } from "../redux/slice/courseSlice";
 
 const Homepage = () => {
-    const [data, setData] = useState<PaginatedCourse>();
+  const courses = useAppSelector(coursesSelector.selectAll);
+  const dispatch = useAppDispatch();
+  const { coursesLoaded } = useAppSelector((state) => state.course);
 
-    useEffect(() => {
-        agent.Courses.list().then((response) => {
-            setData(response);
-        });
-    }, []);
+  useEffect(() => {
+    if (!coursesLoaded) dispatch(getCoursesAsync());
+  }, [coursesLoaded, dispatch]);
 
-    return <div className="course">
-        <div className="course__header">
-            <h1>What to learn next?</h1>
-            <h2>New Courses picked just for you...</h2>
-        </div>
-        <Row gutter={[24, 32]}>
-            {data && data.data.map((course: Course, index: number) => {
-                return <ShowCourses key={index} course={course}/> 
-            })}
-        </Row>
+  return (
+    <div className="course">
+      <div className="course__header">
+        <h1>What to learn next?</h1>
+        <h2>New Courses picked just for you...</h2>
+      </div>
+      <Row gutter={[24, 32]}>
+        {courses &&
+          courses.map((course: Course, index: number) => {
+            return <ShowCourses key={index} course={course} />;
+          })}
+      </Row>
     </div>
+  );
 };
 
 export default Homepage;
