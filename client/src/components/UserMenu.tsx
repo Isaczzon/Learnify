69a219b9fc2d9e864df302edd8f255d175c00adc
Isac <isac.gustafsson.syne21lin@tucsweden.se>
2022-09-26 00:31:08 +0200

    Created UserMenu.tsx and Dashboard.tsx, added it to navigation, some styling for it, implemented logic for signout in userSlice.ts and added the route in App.tsx

---
+++ client/src/components/UserMenu.tsx
@@ -0,0 +1,35 @@
+import { Dropdown, Menu } from "antd";
+import { Link, useHistory } from "react-router-dom";
+import { signOut } from "../redux/slice/userSlice";
+import { useAppDispatch } from "../redux/store/configureStore";
+
+const UserMenu = () => {
+
+    const dispatch = useAppDispatch();
+    const history = useHistory();
+
+    const signout = () => {
+        dispatch(signOut());
+        history.push("/");
+    };
+
+    const menu = (
+    <Menu>
+           <Menu.Item>
+            <Link to="/profile">Profile</Link>
+           </Menu.Item>
+           <Menu.Item>
+            <div onClick={signout}>Logout</div>
+           </Menu.Item>
+    </Menu>
+    );
+    return (
+
+
+        <Dropdown overlay={menu} placement="bottomCenter">
+            <div className="dropdown">Menu</div>
+        </Dropdown>
+    );
+};
+
+export default UserMenu;
