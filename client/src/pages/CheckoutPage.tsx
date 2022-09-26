dbdf63b8f8c1b84d0192811236bf63bdf7c6f390
Isac <isac.gustafsson.syne21lin@tucsweden.se>
2022-09-26 14:41:10 +0200

    Forgot to add stripe-api-key

--- client/src/pages/CheckoutPage.tsx
+++ client/src/pages/CheckoutPage.tsx
@@ -2,7 +2,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import Checkout from '../components/Checkout';
 
-const stripePromise = loadStripe('your stripe publishable key');
+const stripePromise = loadStripe("pk_test_51LmDPhAFoiC8oPIFM17PXiyNNAH4Gh9gWSGSA5K4THSwMnNb6WUZjNEa8dBKdkVtkoLSk4LktsvLFYh8399JgXXY00Bgs5Tkxq");
 
 export default function CheckoutWrapper() {
   return (
