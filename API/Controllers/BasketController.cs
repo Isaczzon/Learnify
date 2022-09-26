193b6cce65ecf4225cce58ef0674a5aa7ce4e832
Isac <isac.gustafsson.syne21lin@tucsweden.se>
2022-09-26 10:21:30 +0200

    Remade some changes in the basket logic

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -26,7 +26,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<BasketDto>> GetBasket()
         {
-            var basket = await ExtractBasket();
+            var basket = await ExtractBasket(GetClientId());
 
             if (basket == null) return NotFound(new ApiResponse(404));
 
@@ -38,7 +38,7 @@ namespace API.Controllers
         [HttpPost]
         public async Task<ActionResult<BasketDto>> AddItemToBasket(Guid courseId)
         {
-            var basket = await ExtractBasket();
+            var basket = await ExtractBasket(GetClientId());
 
             if (basket == null) basket = CreateBasket();
             var course = await _context.Courses.FindAsync(courseId);
@@ -57,7 +57,7 @@ namespace API.Controllers
         [HttpDelete]
         public async Task<ActionResult> RemoveBasketItem(Guid courseId)
         {
-            var basket = await ExtractBasket();
+            var basket = await ExtractBasket(GetClientId());
 
             if (basket == null) return NotFound(new ApiResponse(404));
 
@@ -72,21 +72,37 @@ namespace API.Controllers
 
         private Basket CreateBasket()
         {
-            var clientId = Guid.NewGuid().ToString();
-            var options = new CookieOptions { IsEssential = true, Expires = DateTime.Now.AddDays(10) };
-            Response.Cookies.Append("clientId", clientId, options);
+            var clientId = User.Identity?.Name;
+            
+            if (string.IsNullOrEmpty(clientId))
+            {
+                clientId = Guid.NewGuid().ToString();
+                var options = new CookieOptions { IsEssential = true, Expires = DateTime.Now.AddDays(10) };
+                Response.Cookies.Append("clientId", clientId, options);
+            }
+
             var basket = new Basket { ClientId = clientId };
             _context.Baskets.Add(basket);
             return basket;
         }
 
-        private async Task<Basket> ExtractBasket()
+        private async Task<Basket> ExtractBasket(string clientId)
         {
+            if (string.IsNullOrEmpty(clientId))
+            {
+                Response.Cookies.Delete("clientId");
+                return null;
+            }
             return await _context.Baskets
                         .Include(b => b.Items)
                         .ThenInclude(i => i.Course)
                         .OrderBy(i => i.Id)
-                        .FirstOrDefaultAsync(x => x.ClientId == Request.Cookies["clientId"]);
+                        .FirstOrDefaultAsync(x => x.ClientId == clientId);
+        }
+
+        private string GetClientId()
+        {
+            return User.Identity?.Name ?? Request.Cookies["clientId"];
         }
     }
 }
