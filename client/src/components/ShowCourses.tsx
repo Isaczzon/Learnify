6455151190e4b9ed0497054ab804dd682c3fa891
Isac <isac.gustafsson.syne21lin@tucsweden.se>
2022-09-28 12:11:39 +0200

    Made the users bought courses visable, added the appsettings file and added some styling to it

--- client/src/components/ShowCourses.tsx
+++ client/src/components/ShowCourses.tsx
@@ -11,9 +11,11 @@ interface Props {
 }
 
 const ShowCourses = ({ course }: Props) => {
+  
   const [spanVal, setSpanVal] = useState<number>();
-
   const {basket} = useAppSelector(state => state.basket);
+  const { userCourses } = useAppSelector((state) => state.user);
+
   const dispatch = useAppDispatch();
 
   const checkWidth = (): void => {
@@ -63,19 +65,22 @@ const ShowCourses = ({ course }: Props) => {
           </div>
           <div className="course__bottom">
             <div className="course__bottom__price">{course.price}</div>
-            {basket?.items.find((item) => item.courseId === course.id) !==
-            undefined ? (
-              <Link to="/basket">
-                <div className="course__bottom__cart">Go to Cart</div>
-              </Link>
-            ) : (
-              <div
-                onClick={() => dispatch(addBasketItemAsync({courseId: course.id}))}
-                className="course__bottom__cart"
-              >
-                Add to Cart
-              </div>
-            )}
+            {userCourses?.find((item) => item.id === course.id) !==
+              undefined ? (
+                  <div className="course__bottom__cart">Go to Course</div>
+              ) : basket?.items.find((item) => item.courseId === course.id) !==
+              undefined ? (
+                <Link to="/basket">
+                  <div className="course__bottom__cart">Go to Cart</div>
+                </Link>
+              ) : (
+                <div
+                  onClick={() => dispatch(addBasketItemAsync({courseId: course.id}))}
+                  className="course__bottom__cart"
+                >
+                  Add to Cart
+                </div>
+              )}
           </div>
         </Card>
       </Col>
@@ -84,3 +89,4 @@ const ShowCourses = ({ course }: Props) => {
 };
 
 export default ShowCourses;
+
