3250040d7789052bfdec6b8016b91d71edc3c4e0
Isac <isac.gustafsson.syne21lin@tucsweden.se>
2022-10-06 17:29:50 +0200

    Bug for import of logo in navigation

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -2,7 +2,7 @@ import React, { ChangeEvent, SyntheticEvent, useState } from "react";
 import * as FaIcons from "react-icons/fa";
 import { useDispatch } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
-import Logo from "../assets/logo-Learnify.png";
+import Logo from "../Assets/logo-Learnify.png";
 import { removeBasket } from "../redux/slice/basketSlice";
 import { setCourseParams } from "../redux/slice/courseSlice";
 import { signOut } from "../redux/slice/userSlice";
